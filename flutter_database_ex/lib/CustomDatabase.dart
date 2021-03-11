bce5be6ad102f58b3c4574d4bdad24c7ddb39d7c
MarioEberth <marioeberth@gmail.com>
2021-03-11 16:32:22 +0100

    update comments

--- flutter_database_ex/lib/CustomDatabase.dart
+++ flutter_database_ex/lib/CustomDatabase.dart
@@ -6,6 +6,7 @@ import 'package:flutter/material.dart';
 class CustomData extends StatefulWidget {
   CustomData({this.app});
 
+  ///add Firebase
   final FirebaseApp app;
 
   @override
@@ -13,11 +14,9 @@ class CustomData extends StatefulWidget {
 }
 
 class _CustomDataState extends State<CustomData> {
-  final referenceDatabase = FirebaseDatabase.instance;
-
-  final user = 'User';
+  ///add a Controller to can access the input text of the textField
   final editTextController = TextEditingController();
-
+  ///add a global userDb variable
   DatabaseReference userDb;
 
   @override
@@ -29,7 +28,6 @@ class _CustomDataState extends State<CustomData> {
 
   @override
   Widget build(BuildContext context) {
-    final ref = referenceDatabase.reference();
     return Scaffold(
       appBar: AppBar(
         title: Text('PersonList'),
@@ -44,6 +42,7 @@ class _CustomDataState extends State<CustomData> {
                 height: MediaQuery.of(context).size.height,
                 child: Column(
                   children: [
+                    ///add a Header Text Widget with a padding
                     Container(
                       padding: const EdgeInsets.all(32.0),
                       child: Text(
@@ -53,12 +52,12 @@ class _CustomDataState extends State<CustomData> {
                             TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
                       ),
                     ),
+                    ///add a input TextField Widget where the user can type in a name
                     Container(
                       decoration: BoxDecoration(
                         color: Colors.blueAccent,
                         borderRadius:  BorderRadius.circular(32),
                       ),
-                      margin: const EdgeInsets.all(8.0),
                       child: TextField(
                         controller: editTextController,
                         decoration: InputDecoration(
@@ -70,19 +69,22 @@ class _CustomDataState extends State<CustomData> {
                         ),
                       ),
                     ),
+                    ///Add a Button where the user can push up a Person object to Firebase db
                     TextButton(
                         style: TextButton.styleFrom(
                             backgroundColor: Colors.red,
                             primary: Colors.white),
                         onPressed: () {
+                          ///create a Person
                           Person person = new Person(editTextController.text, true);
-                          ref.child('Users')
-                              .push()
-                              .child(user)
-                              .set(person.toJson())
-                              .asStream();
+                          ///push it to Firebase db
+                          userDb.push()
+                              .child('User')
+                              .set(person.toJson());
+                          ///clear the input text field
                           editTextController.clear();
                         },
+                        ///give the button an Text
                         child: Text('add User')),
                     Flexible(
                         child: firebaseList()
@@ -97,19 +99,29 @@ class _CustomDataState extends State<CustomData> {
     );
   }
 
+  ///function which returns a FirebaseAnimatedList
   Widget firebaseList(){
     return new FirebaseAnimatedList(
+      ///If the scroll view does not shrink wrap, then the scroll view will expand
+      ///to the maximum allowed size in the scrollDirection. If the scroll view
+      /// has unbounded constraints in the scrollDirection, then shrinkWrap must be true.
         shrinkWrap: true,
+        ///add the firebase query
         query: userDb,
+        ///Called, as needed, to build list item widgets.
+        ///List items are only built when they're scrolled into view.
         itemBuilder: (BuildContext context,
             DataSnapshot snapshot,
             Animation<double> animation,
             int index) {
+          ///convert the db result to an person object
           Person person = Person.fromJson(snapshot.value['User']);
+          ///A single fixed-height row that typically contains some text as well as a leading or trailing icon.
           return new ListTile(
               trailing: IconButton(
                 icon: Icon(Icons.delete),
                 onPressed: () =>
+                ///if pressed delete this item in the app and on db
                     userDb.child(snapshot.key).remove(),
               ),
               title: new Text("Name: ${person.name}"),
@@ -119,20 +131,27 @@ class _CustomDataState extends State<CustomData> {
   }
 }
 
-
+///Person object
 class Person{
   final String name;
   final bool isGay;
 
+  ///normal constructor
   Person(this.name, this.isGay);
+  ///constructor for parsing from a json
+  ///is used in factory below
   Person.json({this.name, this.isGay});
 
+  ///from object to json
+  ///is used to push the data up to the db
   Map<String, dynamic> toJson() =>
       {
         'name': name,
         'isGay': isGay,
       };
 
+  ///from json to object
+  ///is used to parse the data from json to object
   factory Person.fromJson(Map<dynamic, dynamic> json) {
     return Person.json(
       name: json['name'] as String,
