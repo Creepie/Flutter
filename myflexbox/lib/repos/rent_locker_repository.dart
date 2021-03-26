c5f9c39293c4ac1a344faeeb01cc27f799bf674c
MarioEberth <marioeberth@gmail.com>
2021-03-26 12:38:56 +0100

    add getFilteredLocker api

--- myflexbox/lib/repos/rent_locker_repository.dart
+++ myflexbox/lib/repos/rent_locker_repository.dart
@@ -5,17 +5,12 @@ import 'package:flutter/material.dart';
 import 'models/locker.dart';
 
 class RentLockerRepository {
+  final String apiKey = "Basic 77+977+90IcGI++/vVQhWjDvv73vv70R77+9Nh/vv70yVTIoe++/vRDvv71WVwBd77+9";
 
   Future<List<Locker>> getLockers() async {
     var url = Uri.https(
         'dev-myflxbx-rest.azurewebsites.net', '/api/1/lockers', {'q': '{https}'});
-    final response = await http.get(
-      url,
-      headers: {
-        HttpHeaders.authorizationHeader:
-        "Basic 77+977+90IcGI++/vVQhWjDvv73vv70R77+9Nh/vv70yVTIoe++/vRDvv71WVwBd77+9"
-      },
-    );
+    final response = await http.get(url, headers: {HttpHeaders.authorizationHeader: apiKey},);
 
     if (response.statusCode == 200) {
       List<Locker> list = json
@@ -28,6 +23,24 @@ class RentLockerRepository {
       // then throw an exception.
       return null;
     }
-
   }
+
+  Future<List<Locker>> getFilteredLockers(int size, String startTime, String endTime, double lat, double long) async {
+    var url = Uri.https(
+        'dev-myflxbx-rest.azurewebsites.net', '/api/1/compartments?size=$size&startTime=$startTime&endTime=$endTime&limit=10&latitude=$lat&longitude=$long', {'q': '{https}'});
+    final response = await http.get(url, headers: {HttpHeaders.authorizationHeader: apiKey},);
+
+    if (response.statusCode == 200) {
+      List<Locker> list = json
+          .decode(response.body)['lockers']
+          .map((data) => Locker.fromJson(data))
+          .toList().cast<Locker>();
+      return list;
+    } else {
+      // If the server did not return a 200 OK response,
+      // then throw an exception.
+      return null;
+    }
+  }
+
 }
