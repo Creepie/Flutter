a8474146a1eb4af79ca7f09de894f5db2965ca94
MarioEberth <marioeberth@gmail.com>
2021-04-10 19:29:40 +0200

    add db functions for contact_screen.dart

--- myflexbox/lib/Screens/profile/widgets/contact_screen.dart
+++ myflexbox/lib/Screens/profile/widgets/contact_screen.dart
@@ -1,8 +1,11 @@
 import 'package:contacts_service/contacts_service.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:myflexbox/config/app_router.dart';
 import 'package:myflexbox/config/size_config.dart';
 import 'package:myflexbox/config/constants.dart';
+import 'package:myflexbox/repos/models/user.dart';
 
 
 class ContactScreen extends StatelessWidget {
@@ -64,6 +67,70 @@ class _ContactsState extends State<Contacts> {
       return m[0] == "+" ? "+" : "";
     });
   }
+
+  Future<List<DBUser>> getDBContact(String phoneNumber) async{
+
+    int count = 0;
+    List<DBUser> dbContacts = [];
+    FirebaseDatabase database = FirebaseDatabase();
+    DatabaseReference userDb = database.reference().child('Users');
+    var myUserId = FirebaseAuth.instance.currentUser.uid;
+
+    //get my current user from db
+    DBUser myUser = await getUserFromDB(myUserId);
+    //get contacts list from search with the given phoneNumber
+    DataSnapshot contact = await userDb.orderByChild('number').equalTo(phoneNumber).once();
+
+    //if search not null > add all contacts to user favourites db and to list for favourites list
+    if(contact.value != null){
+      Map<dynamic, dynamic>.from(contact.value).forEach((key,values) {
+        var user = DBUser.fromJson(values);
+        if(!myUser.favourites.contains(user.uid)){
+          myUser.favourites.add(user.uid);
+          dbContacts.add(user);
+          count++;
+        }
+      });
+
+      //save updated myUser to db
+      if(count > 0){
+        var test = await userDb.child(myUser.uid).set(myUser.toJson());
+      }
+    }
+    return Future.value(dbContacts);
+  }
+
+  
+  Future<DBUser> getUserFromDB(String uid) async {
+    FirebaseDatabase database = FirebaseDatabase();
+    DatabaseReference userDb = database.reference().child('Users');
+    DataSnapshot user = await userDb
+        .child(uid)
+        .once();
+    if(user.value != null){
+      return Future.value(DBUser.fromJson(user.value));
+    } else {
+      return Future.value(null);
+    }
+  }
+
+  Future<List<Contact>> getFavouriteUsers() async {
+    var myUserId = FirebaseAuth.instance.currentUser.uid;
+    DBUser myUser = await getUserFromDB(myUserId);
+
+    List<Contact> users =[];
+
+    for(int i=0; i< myUser.favourites.length; i++ ){
+      DBUser user = await getUserFromDB(myUser.favourites[i]);
+      if(user != null){
+
+        users.add(Contact(displayName: user.number, givenName: user.name ));
+      }
+    }
+
+    return Future.value(users);
+  }
+
 
   /// get all contacts that are on the phone
   getAllContacts() async {
@@ -154,25 +221,7 @@ class _ContactsState extends State<Contacts> {
         child: Icon(Icons.add),
         backgroundColor: Theme.of(context).primaryColor,
         onPressed: () async {
-          try {
-            Contact contact = await ContactsService.openContactForm();
-
-            // firebase durchsuchen nach telnr.
-            // falls gefunden User in firebase kontakte sichern
-            // zu favoriten
-
-            if(contact != null){
-              getAllContacts();
-            }
-
-          } on FormOperationException catch (e) {
-            switch(e.errorCode) {
-              case FormOperationErrorCode.FORM_OPERATION_CANCELED:
-              case FormOperationErrorCode.FORM_COULD_NOT_BE_OPEN:
-              case FormOperationErrorCode.FORM_OPERATION_UNKNOWN_ERROR:
-                print(e.toString());
-            }
-          }
+         getFavouriteUsers();
         },
       ),
       /// widget that combines common paiinting, positioning and sizing widgets
