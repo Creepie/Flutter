c252d8ccdc13158127e5df786b9a45bb037a7996
JustMoe <62350723+JustMoe@users.noreply.github.com>
2021-07-02 14:58:06 +0200

    added text to empty locker list

--- myflexbox/lib/Screens/current_lockers/widgets/current_locker_empty.dart
+++ myflexbox/lib/Screens/current_lockers/widgets/current_locker_empty.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class EmptyScreen extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
-    return Center();
+    return Center(
+      child: Text("noch keine Einträge vorhanden", style: TextStyle(
+        fontSize: 16,
+        color: Colors.grey
+      ),),
+    );
   }
 
 }
